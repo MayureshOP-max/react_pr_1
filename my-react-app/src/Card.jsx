ebc28941a8cb3f5dd3098296633df9a7ddf75b05
MayureshOP-max <mayureshjaywant123@gmail.com>
2024-03-31 13:33:12 +0530

    1

---
+++ my-react-app/src/Card.jsx
@@ -0,0 +1,12 @@
+
+function Card(){
+
+    return(
+        <div className="card">
+            <img src="" alt="" />
+            <h2>Gaming</h2>
+            <p>I am learning React.js and play video games.</p>
+        </div>
+    );
+}
+export default Card
--- my-react-app/src/Footer.jsx
+++ my-react-app/src/Footer.jsx
@@ -2,7 +2,7 @@
 function Footer(){
     return(
         <footer>
-            <p>&copy; Your Website Name</p>
+            <p>&copy; {new Date().getFullYear()} Your Website Name</p>
         </footer>
     );
 }
